9726756511e397128f6dbe163abc36dbaaf0c9a4
joliebao <142067108+joliebao@users.noreply.github.com>
2026-02-04 23:18:36 -0500

    Job post page done
    Added composable functions for creating and displaying posts, including layout and input fields.

--- app/src/main/java/com/example/probonoapp/MainActivity.kt
+++ app/src/main/java/com/example/probonoapp/MainActivity.kt
@@ -10,6 +10,27 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.probonoapp.ui.theme.ProBonoAppTheme
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,6 +53,75 @@ fun MainLayout() {
             JobsPageLayout()
         }
     }
+}
+
+@Composable
+fun makePost(): Post {
+    val p = Post()
+    Column() {
+        postInformation("Subject", p)
+        Spacer(modifier = Modifier.size(10.dp))
+        postInformation("Classification", p)
+        Spacer(modifier = Modifier.size(10.dp))
+        postInformation("Date", p)
+        Spacer(modifier = Modifier.size(10.dp))
+        postInformation("Description", p)
+    }
+    return p
+}
+
+@Composable()
+fun postVisuals(p: Post){
+    Column() {
+        Text(
+            text = p.title,
+            fontSize = 30.sp,
+        )
+        Row(horizontalArrangement = Arrangement.Center) {
+            Text(
+                text = "Needed by: " + p.date,
+                fontSize = 15.sp,
+            )
+            Spacer(modifier = Modifier.size(20.dp))
+            Text(
+                text = "Case Category: " + p.category,
+                fontSize = 15.sp,
+            )
+        }
+        Text(
+            text = p.description,
+            fontSize = 20.sp,
+            modifier = Modifier.clip(RoundedCornerShape(16.dp))
+            .background(Color(46, 74, 120))
+        )
+    }
+}
+
+@Composable
+fun JobsPageLayout(posts: MutableList<Post>){
+    LazyColumn(modifier = Modifier.padding(16.dp)){
+        items(posts.size) { i ->
+            postVisuals(posts.get(i))
+        }
+    }
+}
+
+@Composable
+fun postInformation(prompt: String, p: Post) {
+    var text by remember { mutableStateOf("") }
+    when (prompt){
+        "title" -> p.title = text
+        "classification" -> p.category = text
+        "date" -> p.date = text
+        "description" -> p.description = text
+    }
+    TextField(
+        value = text,
+        onValueChange = { newText ->
+            text = newText
+        },
+        label = { Text(prompt) },
+    )
 }
 
 @Preview(showSystemUi = true)
