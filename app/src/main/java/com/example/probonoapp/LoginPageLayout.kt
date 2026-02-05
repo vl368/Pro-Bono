6addd30909c59562d01494643c103f3ab29dae87
ly <everythingbagel@Lyras-MacBook-Air.local>
2026-02-05 03:15:11 -0500

    Login Page Updates

--- app/src/main/java/com/example/probonoapp/LoginPageLayout.kt
+++ app/src/main/java/com/example/probonoapp/LoginPageLayout.kt
@@ -5,6 +5,9 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
+import androidx.compose.material3.Switch
+import androidx.compose.material3.SwitchColors
+import androidx.compose.material3.SwitchDefaults
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
@@ -14,32 +17,65 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 
 @Composable
-fun LoginPageLayout(){
-    var experience = false
+fun LoginPageLayout(navController: NavController){
     Column(horizontalAlignment = Alignment.CenterHorizontally) {
         var userName by remember { mutableStateOf("") }
         var password by remember { mutableStateOf("") }
+        var isClient by remember { mutableStateOf(true) }
+        val legalBlue1 = Color(52, 51, 101)
+        val legalBlue2 = Color(0, 100, 160)
 
         Spacer(modifier = Modifier.size(30.dp))
         Image(
             painter = painterResource(id = R.drawable.pro_bono),
             contentDescription = "Logo"
         )
+
         createTextField("Username", userName) { userName = it }
         Spacer(modifier = Modifier.size(10.dp))
         createTextField("Password", password) { password = it }
         Spacer(modifier = Modifier.size(30.dp))
-        Button(onClick = {experience = !experience}){
-            Text(
-                text = if (!experience) "Legal Advisor Mode" else "User Mode"
+        ClassicText("I am a", 20, legalBlue1)
+        ClassicText("${if (isClient) "client" else "volunteer"}", 25, legalBlue2)
+        Switch(
+            checked = isClient,
+            onCheckedChange = {
+                isClient = !isClient
+            },
+            colors = SwitchDefaults.colors(
+                checkedThumbColor = legalBlue2, // Color of the thumb when checked
+                checkedTrackColor = legalBlue1,   // Color of the track when checked
+                uncheckedThumbColor = Color.LightGray, // Color of the thumb when unchecked
+                uncheckedTrackColor = Color.DarkGray
             )
-        }
+        )
     }
+}
+
+@Composable
+fun ClassicText(text: String, fontSize: Int, color: Color) {
+    Text(
+        text = text,
+        fontSize = fontSize.sp,
+        fontWeight = FontWeight.SemiBold,
+        color = color,
+        fontFamily = FontFamily.Serif,
+        letterSpacing = 0.1.sp
+    )
 }
 
 @Composable
@@ -56,5 +92,6 @@ fun createTextField(prompt: String, value: String, changeVal: (String) -> Unit) 
 @Preview(showSystemUi = true)
 @Composable
 fun PreviewLogin() {
-    LoginPageLayout()
+    val navController = rememberNavController()
+    LoginPageLayout(navController)
 }
--- app/src/main/java/com/example/probonoapp/MainActivity.kt
+++ app/src/main/java/com/example/probonoapp/MainActivity.kt
@@ -9,28 +9,9 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.example.probonoapp.ui.theme.ProBonoAppTheme
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.platform.LocalContext
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -44,10 +25,15 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MainLayout() {
+    val context = LocalContext.current
+    val coroutineScope = rememberCoroutineScope()
+    val dataManager = remember { DataManager(context) }
     val navController = rememberNavController()
     NavHost(navController = navController, startDestination = "login_page") {
         composable("login_page") {
-            LoginPageLayout()
+            LoginPageLayout(
+                navController = navController
+            )
         }
         composable("jobs_page") {
             JobsPageLayout(
