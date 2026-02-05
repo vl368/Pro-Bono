46af0e4a75852021d8613eafab6beddf52e296c7
joliebao <142067108+joliebao@users.noreply.github.com>
2026-02-05 01:06:03 -0500

    LoginPageLayout

--- app/src/main/java/com/example/probonoapp/LoginPageLayout.kt
+++ app/src/main/java/com/example/probonoapp/LoginPageLayout.kt
@@ -3,5 +3,22 @@ package com.example.probonoapp
 import androidx.compose.runtime.Composable
 
 @Composable
-fun LoginPageLayout() {
+fun LoginPageLayout(){
+    var experience = false
+    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+        Spacer(modifier = Modifier.size(30.dp))
+        Image(
+            painter = painterResource(id = R.drawable.logo),
+            contentDescription = "Logo"
+        )
+        accountInformation("Username")
+        Spacer(modifier = Modifier.size(10.dp))
+        accountInformation("Password")
+        Spacer(modifier = Modifier.size(30.dp))
+        Button(onClick = {experience = !experience}){
+            Text(
+                text = if (!experience) "Legal Advisor Mode" else "User Mode"
+            )
+        }
+    }
 }
